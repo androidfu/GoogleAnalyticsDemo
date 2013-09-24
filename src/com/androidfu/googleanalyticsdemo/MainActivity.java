c7df656af82743add545b47d4ebe507c14b71f6e
Bill Mote <bill.mote@rockfishinteractive.com>
2013-09-24 14:47:45 -0400

    initial commit

---
+++ src/com/androidfu/googleanalyticsdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.androidfu.googleanalyticsdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import com.androidfu.library.googleanalytics.AnalyticsUtils;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Track PageView
+        AnalyticsUtils.getInstance(this).trackPageView("path");
+
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // Track Event
+                AnalyticsUtils.getInstance(getApplicationContext()).trackEvent("category", "action", "label", 0);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
