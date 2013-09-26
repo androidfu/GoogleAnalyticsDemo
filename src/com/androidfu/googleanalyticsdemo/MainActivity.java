3c6bcfb4ccc2789cd4af0882eb44197ecc993f30
Bill Mote <bill.mote@rockfishinteractive.com>
2013-09-26 10:19:05 -0400

    Comment cleanup.

--- src/com/androidfu/googleanalyticsdemo/MainActivity.java
+++ src/com/androidfu/googleanalyticsdemo/MainActivity.java
@@ -15,9 +15,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Setup Tracker with context and our key.
-        //AnalyticsUtils.configure(getApplication(), GOOGLE_ANALYTICS_KEY);
-
         // Track PageView
         AnalyticsUtils.getInstance().trackPageView("path");
 
